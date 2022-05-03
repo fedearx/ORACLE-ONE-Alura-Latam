fc7653cf4f1f5dff715831271a22acb01097c3ec
Federico A. Rubio <fedearx@gmail.com>
2022-05-02 23:51:31 -0300

    02. Comportamiento del formulario - 03. Capturando el valor del input

--- Front-end-ONE/04.Sitios-interactivos/01.Manipulacion-del-DOM-con-Js/script.js
+++ Front-end-ONE/04.Sitios-interactivos/01.Manipulacion-del-DOM-con-Js/script.js
@@ -1,7 +1,7 @@
 const btn = document.querySelector("[data-form-btn]");
 
-console.log(btn);
 
 btn.addEventListener("click", function () {
-  console.log("crear tarea");
+  const input = document.querySelector("[data-form-input]");
+  console.log(input.value);
 })
