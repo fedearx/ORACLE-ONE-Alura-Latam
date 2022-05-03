2486d2434e87522fcd0ab27e2514eb279afec170
Federico A. Rubio <fedearx@gmail.com>
2022-05-03 00:12:03 -0300

    03.Agregar una tarea a la lista - 02.Agregando tarea

--- Front-end-ONE/04.Sitios-interactivos/01.Manipulacion-del-DOM-con-Js/script.js
+++ Front-end-ONE/04.Sitios-interactivos/01.Manipulacion-del-DOM-con-Js/script.js
@@ -5,7 +5,19 @@ const createTask = (evento) => {
   evento.preventDefault();          // detiene el funcionamiento por defecto
 
   const input = document.querySelector("[data-form-input]");
-  console.log(input.value);
+  const value = input.value;
+  const task = document.querySelector("[data-task]");
+  input.value = '';
+  // backticks
+  const content = `
+  <div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`
+  task.innerHTML = content;         // lo copio en el html
+
+  console.log(value);
 }
 
 
