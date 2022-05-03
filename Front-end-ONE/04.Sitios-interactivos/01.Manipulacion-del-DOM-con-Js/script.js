34d91e81c3bd3bfd7c6767743c5023d9bf297e61
Federico A. Rubio <fedearx@gmail.com>
2022-05-03 00:56:12 -0300

    04.Concluir una tarea - 03.Concluir la tarea

--- Front-end-ONE/04.Sitios-interactivos/01.Manipulacion-del-DOM-con-Js/script.js
+++ Front-end-ONE/04.Sitios-interactivos/01.Manipulacion-del-DOM-con-Js/script.js
@@ -45,10 +45,23 @@ btn.addEventListener("click", createTask);
 
 const checkComplete = () => {
   const i = document.createElement('i');
-  i.classList.add('far');
-  i.classList.add('fa-check-square');
-  i.classList.add('icon');
-
+  i.classList.add('far', 'fa-check-square', 'icon');
+//  i.classList.add('fa-check-square');
+//   i.classList.add('icon');
+i.addEventListener('click', completeTask);
   return i;
 }
+
+
+const completeTask = (event) => {
+  const element = event.target;
+  element.classList.toggle('fas');              // toggle si no existe la crea, si existe la saca
+  element.classList.toggle('completeIcon');    // esta en ccs y da el color azul
+  element.classList.toggle('far');
+
+}
+
+
+
+
 
