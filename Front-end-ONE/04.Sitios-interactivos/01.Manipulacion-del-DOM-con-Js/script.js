1ce72187d61aac867023070fe30d6e40c3a6135c
Federico A. Rubio <fedearx@gmail.com>
2022-05-03 00:00:41 -0300

    02. Comportamiento del formulario - 05. Comportamiento del formulario - Clase02 completada.

--- Front-end-ONE/04.Sitios-interactivos/01.Manipulacion-del-DOM-con-Js/script.js
+++ Front-end-ONE/04.Sitios-interactivos/01.Manipulacion-del-DOM-con-Js/script.js
@@ -1,7 +1,15 @@
 const btn = document.querySelector("[data-form-btn]");
 
+const createTask = (evento) => {
 
-btn.addEventListener("click", function () {
+  evento.preventDefault();          // detiene el funcionamiento por defecto
+
   const input = document.querySelector("[data-form-input]");
   console.log(input.value);
-})
+}
+
+
+//  funcion normal
+// btn.addEventListener("click", function (evento) {
+// funcion flecha o funcion anonima
+btn.addEventListener("click", createTask);
